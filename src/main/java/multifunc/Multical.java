5e90dc5dd9316b40b07b34cf33a9e5daa66a9781
sada <dsfsafsdfsfsf>
2016-04-27 16:04:01 +0900

    multi

---
+++ src/main/java/multifunc/Multical.java
@@ -0,0 +1,5 @@
+package multifunc;
+
+public class Multical {
+
+}
---
+++ src/test/java/multifunc/Multicaltest.java
@@ -0,0 +1,14 @@
+package multifunc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Multicaltest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
